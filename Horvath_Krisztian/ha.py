b2a030f75e4c1ef03f0ce8027a01cbb67963b825
HorvathDezsoKrisztia <dezsokrisztian.horvath@katonaj-mkszig.hu>
2022-09-16 12:27:40 +0200

    ha 2

--- Horvath_Krisztian/ha.py
+++ Horvath_Krisztian/ha.py
@@ -1,4 +1,8 @@
 szam = int (input("adj meg egy számot! "))
 if szam < 0:
     print("a szám negativ")
+elif szam == 0:
+    print("a szám nulla")
+else:
+    print("a szám poztiv")
 print("program vége!")
