d8159c2e8c3c32f706595538fecae1022a30ffa2
Csapo Levente <levente.csapo@katonaj-mkszig.hu>
2022-09-19 09:16:19 +0200

    logikai1

--- Csapo_Levente/logikai1.py
+++ Csapo_Levente/logikai1.py
@@ -2,5 +2,5 @@ szam = int(input("Adj meg egy számot! "))
 
 if  szam % 2 == 0 and szam > 0:
     print(szam, "pozitiv es paros")
-else:
+elif szam % 2 == 1 and szam < 0:
     print(szam, "negativ paratlan")
