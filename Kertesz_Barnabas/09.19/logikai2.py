80a8f955ee9908c48b9c454793727ff0309c3a64
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-19 09:29:49 +0200

    Feladat 2 V0.1

--- Kertesz_Barnabas/09.19/logikai2.py
+++ Kertesz_Barnabas/09.19/logikai2.py
@@ -1,14 +1,10 @@
 szoveg1 = input("Jön Henrik  ma kosarazni? (I/N) ")
 szoveg2 = input("Jön Hanna  ma kosarazni? (I/N) ")
-# 2 Yes
 if szoveg1 == 'I' and szoveg2 == 'I':
     print("Mindketten jönnek kosarazni")
-# 2 No
 if szoveg1 == 'N' and szoveg2 == 'N':
     print("Senki nem jön kosarazni")
-# 1 Yes or 1 No
 if szoveg1 == 'I' and szoveg2 == 'N':
     print("Csak Henrik jön kosarazni")
-# 1 No or 1 Yes
 if szoveg1 == 'N' and szoveg2 == 'I':
     print("Csak Hanna jön kosarazni")
