9939507f912c356a00fa781f2c603208a1c9dd94
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-19 09:13:48 +0200

    Feladat

---
+++ Kertesz_Barnabas/09.19/feladat2.py
@@ -0,0 +1,10 @@
+szam = int(input("Adj meg egy számot: " ))
+if szam > 0:
+    print("A szám pozitív")
+else:
+    print("A szám negatív")
+
+if szam % 2 == 0:
+    print("A szám páros")
+else:
+    print("A szám páratlan")
