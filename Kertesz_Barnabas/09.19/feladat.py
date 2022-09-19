e0c46fa27abf006c5ecfdd549a0afd54431e12de
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-19 08:45:58 +0200

    if/elif/else használata V0.0.4

--- Kertesz_Barnabas/09.19/feladat.py
+++ Kertesz_Barnabas/09.19/feladat.py
@@ -1,8 +1,8 @@
 import random
 
 gondolt_szam = random.randint(1, 5)
-szam = int(input("Adj egy számot: "))
-
+szam = int(input("Adj meg egy számot (1-5 között)!: "))
+print(f'A gondolt szám: {gondolt_szam}')
 if gondolt_szam == szam:
     print("A két szám egyenlő")
 elif gondolt_szam < szam:
