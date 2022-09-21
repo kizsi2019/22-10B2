ce0454787c3fd71646f116584066466c5d11bc7f
Hajdu Dominik Bence <dominikbence.hajdu@katonaj-mkszig.hu>
2022-09-21 08:32:52 +0200

    adatbekeres2

--- Hajdu_Dominik/github1/adatbekeres_while1.py
+++ Hajdu_Dominik/github1/adatbekeres_while1.py
@@ -1,7 +1,6 @@
 
 szam = int(input('Adj meg egy számot 5 és 10 között! '))
 while not 5 <= szam <= 10:
-      szam = int(input('Helytelen érték! Adj meg egy számot 5 és 10 között! '))
-  
+      szam = int(input('Rossz érték! Adj meg egy számot 5 és 10 között! '))  
 print('Rendben!')     
   
