b57cc7461e6fbfcb76fc2af3e86c6ce7b7c7bf4a
Csapo Levente <levente.csapo@katonaj-mkszig.hu>
2022-09-23 11:03:25 +0200

    hazi feladat

--- Csapo_Levente/logikai3.py
+++ Csapo_Levente/logikai3.py
@@ -2,9 +2,9 @@ szam = int(input("Adj meg egy számot! "))
 
 if szam % 3 == 0 and szam % 4 == 0:
     print("mindkettovel oszthato")
-if szam % 3 == 0:
+elif szam % 3 == 0:
     print("3al oszthato")
-if szam % 4 == 0:
+elif szam % 4 == 0:
     print("4el oszthato")
 else:
     print("Nem oszthato semmelyikkel!")
