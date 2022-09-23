777546e84588c19c667ae9a08c3273ec7acb84c0
Eberling Ferenc Tamas <ferenctamas.eberling@katonaj-mkszig.hu>
2022-09-23 12:34:57 +0200

    hazinak

--- Eberling_Ferenc/python/egymasbaagyaztottciklusok2.py
+++ Eberling_Ferenc/python/egymasbaagyaztottciklusok2.py
@@ -1,13 +1,23 @@
-import os
-
-
+szam = int(input("Adj meg egy párosszámot! : "))
 dbkar = 1
 sor = 1
-while sor <=99 :
+
+while sor <= szam :
     oszlop = 1
     while oszlop <= dbkar:
-        print("0123456789", end='')
+        print("0", end='')
         oszlop = oszlop + 1
     print('')
     dbkar = dbkar + 1
     sor = sor + 1
+sor2 = szam
+dbkar2 = szam 
+oszlop2= szam
+while sor2 >= szam :
+    oszlop2 = szam
+    while oszlop2 >= szam:
+        print("0", end='')
+        oszlop2 = oszlop2 - 1
+    print('')
+    dbkar2 = dbkar2 - 1
+    sor2 = sor2 - 1
