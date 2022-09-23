a8c137d5509920eb24f9279378d97a2b275e2aac
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-23 12:13:20 +0200

    While ciklus agyaas

---
+++ Kertesz_Barnabas/09.23/egymasba_agyazot_ciklusok.py
@@ -0,0 +1,10 @@
+darab_karakter = 1
+sor = 1
+while sor <= 88:
+    oszlop = 1
+    while oszlop <= darab_karakter:
+        print('0 ', end='')
+        oszlop = oszlop + 1
+    print('')
+    darab_karakter = darab_karakter + 1
+    sor = sor + 1
