a679d6bfbd07f2b943df0bb12fdab12d2e3f5acb
Csapo Levente <levente.csapo@katonaj-mkszig.hu>
2022-09-23 10:57:46 +0200

    logikai3

--- Csapo_Levente/logikai3.py
+++ Csapo_Levente/logikai3.py
@@ -1,8 +1,8 @@
 szam = int(input("Adj meg egy számot! "))
 
+if szam % 3 == 0 and szam % 4 == 0:
+    print("mindkettovel oszthato")
 if szam % 3 == 0:
     print("3al oszthato")
-elif szam % 4 == 0:
+if szam % 4 == 0:
     print("4el oszthato")
-elif szam % 3 == 0 or szam % 4 == 0:
-    print("mindkettovel oszthato")
