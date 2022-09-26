7dcd121cc1551414d1a28ea81e448aa39e27463e
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-26 09:00:53 +0200

    Házi feladat kész

--- Kertesz_Barnabas/09.23/feladat.py
+++ Kertesz_Barnabas/09.23/feladat.py
@@ -1,3 +1,6 @@
+import os
+
+
 sor = 1
 szam_2 = int(input("Adj egy párosszámot: " ))
 darab_karakter = 1
@@ -9,3 +12,13 @@ while sor <= szam_2/2:
     print('')
     darab_karakter = darab_karakter + 1
     sor = sor + 1
+darab_karakter = szam_2/2
+sor = szam_2
+while sor > 0:
+    oszlop = 1
+    while oszlop <= darab_karakter:
+        print('0 ', end='')
+        oszlop = oszlop + 1
+    print("")
+    darab_karakter = darab_karakter - 1
+    sor = sor - 1
