f063eb2ca97bb34e386bc30de731bbfd245b101a
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-26 09:24:27 +0200

    Feladat 2

---
+++ Kertesz_Barnabas/09.26/feladat2.py
@@ -0,0 +1,13 @@
+darab_karakter = 1
+sor = 1
+while sor <= 5:
+    oszlop = 1
+    while oszlop <= 5:
+        if darab_karakter == oszlop:
+            print('0 ', end='')
+        else:
+            print(' ', end='')
+        oszlop = oszlop + 1
+    print('')
+    darab_karakter = darab_karakter + 1
+    sor = sor + 1
