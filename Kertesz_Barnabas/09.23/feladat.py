416d543de07a70ad8334af9411155dd7bb39a90a
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-26 08:17:56 +0200

    Házi feladat elkezdés

--- Kertesz_Barnabas/09.23/feladat.py
+++ Kertesz_Barnabas/09.23/feladat.py
@@ -10,14 +10,3 @@ while sor <= szam_2:
     print('')
     darab_karakter = darab_karakter + 1
     sor = sor + 1
-szam_2 = szam_2 * 2
-darab_karakter = 1
-sor = 1
-while sor <= szam_2:
-    oszlop = 1
-    while oszlop <= darab_karakter:
-        print('0 ', end='')
-        oszlop = oszlop - 1
-    print('')
-    darab_karakter = darab_karakter - 1
-    sor = sor - 0
