d50d8c0721dbe19248d86a862114b99e48f46167
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-09-28 08:22:53 +0200

    Lista gyakorlás

--- Kertesz_Barnabas/09.28/lista.py
+++ Kertesz_Barnabas/09.28/lista.py
@@ -1,2 +1,7 @@
 honapok = ['január', 'február', 'március', 'április']
-print(honapok)
+
+print(honapok)  # Adatot írja ki #
+
+print(', '.join(honapok))  # Szövegként írja ki #
+
+print(len(honapok))    # Hosszát írja ki #
