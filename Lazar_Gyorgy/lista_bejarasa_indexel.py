711032de0760ff37e9d10df78de7ca7821879462
LazarGyorgyLevente <gyorgylevente.lazar@katonaj-mszig.hu>
2022-10-03 09:39:54 +0200

    listak bejárása

---
+++ Lazar_Gyorgy/lista_bejarasa_indexel.py
@@ -0,0 +1,6 @@
+honapok = ['január', 'február', 'mardcius', 'április', 'május']
+
+index = 0
+for honap in honapok:
+    print(index, honap)
+    index = index + 1 
---
+++ Lazar_Gyorgy/listak_bejarasa_index2.py
@@ -0,0 +1,7 @@
+honapok = ['január', 'február', 'mardcius', 'április', 'május']
+'''
+for index in range(len(honapok)):
+    print(index, honapok[index])
+ '''
+for index, honap in enumerate(honapok):
+    print(index, honap)
