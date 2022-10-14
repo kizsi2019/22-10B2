08fac1cd0bfb7f57d0e1c9c659fc3d29d038d759
Barna09 <barnabas.kertesz@katonaj-mkszig.hu>
2022-10-14 11:27:12 +0200

    Lista gyakorlás

--- Kertesz_Barnabas/10.14/lista_metotudusai.py
+++ Kertesz_Barnabas/10.14/lista_metotudusai.py
@@ -1,4 +1,8 @@
 nyelvek = ["Python", "C", "C++", "C#", "Java"]
-### nyelvek.sort() ###
-nyelvek.reverse()
+### nyelvek.sort()
+### nyelvek.reverse()
+### print(nyelvek)
+### print(nyelvek.index('Python'))
+print(nyelvek.count('Python'))
+nyelvek.append('py')
 print(nyelvek)
