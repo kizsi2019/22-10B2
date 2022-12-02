bd01a95f3072268e13846626be28cc9ffc9b6e85
LazarGyorgyLevente <gyorgylevente.lazar@katonaj-mszig.hu>
2022-12-02 10:29:19 +0100

    szamlalas

---
+++ Lazar_Gyorgy/szoveg_lista1.py
@@ -0,0 +1,7 @@
+szoveg = input("Adj meg egy mondatot")
+if szoveg[-1] == '.':
+    print("Ez a mondat kijelentő!")
+if szoveg[-1] == '?':
+    print("Ez a mondtad kérdő!")
+if szoveg[-1] == '?':
+    print("Ez a mondat felkiáltó/óhajtó!")
