3030fb19556105fa355d0aaaee5334975cf3431b
NagyMateMarcell <matemarcell.nagy@katonaj-mkszig.hu>
2022-12-09 11:02:53 +0100

    orai munka feltoltese

---
+++ Nagy_Marcell/feladat4.py
@@ -0,0 +1,18 @@
+honapok =['január','február', 'március', 'április']
+
+print(honapok)
+print(', '.join(honapok))
+print(len(honapok))
+print(honapok[0])
+print(honapok[-1])
+
+szo = "almafa"
+print(szo[:3])
+
+gyumolcsok = []
+
+gyumolcs = None
+while gyumolcs !='':
+    gyumolcs = input('Adj meg egy gyumolcsot!')
+    if gyumolcs !
+
