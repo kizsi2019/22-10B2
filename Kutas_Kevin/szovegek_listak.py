e5d960db18d29581919a3e3832c21aaeb4d29d72
Csapo Levente <levente.csapo@katonaj-mkszig.hu>
2023-01-11 08:36:56 +0100

    olaszkave

---
+++ Kutas_Kevin/szovegek_listak.py
@@ -0,0 +1,9 @@
+# Szövegek (sztringek) összefűzhetőek
+print('Jöttem' + ' ' + 'láttam' + ' ' + 'győztem.')
+
+# Szövegek (sztringek) szorzásjellel megsokszorozhatóak
+print('ja' + 'j' * 7)
+
+# Más adattípusok szöveggé (sztringgé) alakíthatóak az str() függvénnyel
+print(str(7.53))
+  
