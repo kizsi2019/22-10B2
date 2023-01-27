37679da20bc482f202bcaad24993ff876d394f2d
SoosDaniel <daniel.soos@katonaj-mkszig.hu>
2023-01-27 11:02:20 +0100

    5 fügvény

---
+++ Soos_Daniel/fugveny5.py
@@ -0,0 +1,26 @@
+lista = []
+szam = int(input("Adj meg egy számot"))
+while szam > 0:
+    lista.append((szam))
+    szam = int(input("Adj meg egy számot"))
+def min(lista):
+    min = lista[0]
+    for item in lista:
+        if item < min:
+            min = item
+    return min
+def max(lista):
+    max = lista[0]
+    for item in lista:
+        if item > max:
+            max = item
+    return max
+def osszegzo(lista):
+    osszeg = 0
+    for item in lista:
+        osszeg += item
+    return osszeg
+print(lista)
+print("A lista legkisebb eleme", min(lista))
+print("A lista legnagyobb eleme", max(lista))
+print("A lista összege:",osszegzo(lista))
