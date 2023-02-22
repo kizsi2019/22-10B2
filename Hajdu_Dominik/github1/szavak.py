5cc827e63b2c20581a065f609f4db75a4c8b8f94
Hajdu Dominik Bence <dominikbence.hajdu@katonaj-mkszig.hu>
2023-02-22 11:29:52 +0100

    sssh

---
+++ Hajdu_Dominik/github1/szavak.py
@@ -0,0 +1,7 @@
+szo1 = int(input("adj meg egy szot"))
+szo2= int(input("adj meg egy szot"))
+if szo1 > szo2:
+    print(szo1, "nagyobb mint", szo2)
+elif szo1 < szo2:
+    print(szo1, "kisebb mint", szo2)
+    
