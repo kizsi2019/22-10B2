fe1e8f3d6b263f8f68caae81e5467e2def28abf4
Csapo Levente <levente.csapo@katonaj-mkszig.hu>
2023-03-01 10:21:05 +0100

    oop

---
+++ Csapo_Levente/python/oop3.py
@@ -0,0 +1,34 @@
+import random
+
+class Kor:
+    def __init__(self, sugar, kozeppont=(0, 0)):
+        self.kozeppont = kozeppont
+        self.sugar = sugar
+
+    def terulet(self):
+        return self.sugar * pow(3.14, 2)
+
+    def kerulet(self):
+        return self.sugar * 2 * 3.14
+
+    def info(self):
+        print(f'A(z) {self.sugar} egység sugarú, {self.kozeppont} középpontú kör területe {self.terulet():.2f} egység, kerülete {self.kerulet():.2f} egység.')
+
+kor_01 = Kor(5, (2, 6))
+
+print(kor_01)
+print(type(kor_01))
+print(isinstance(kor_01, Kor))
+
+print(kor_01.terulet())
+
+korok = []
+for _ in range(5):
+    kor = Kor(random.randint(0, 10))
+    korok.append(kor)
+
+for kor in korok:
+    kor.info()
+    
+korok[0].info()
+  
