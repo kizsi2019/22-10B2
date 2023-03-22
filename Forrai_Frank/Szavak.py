d5ac7ac5128415635c334257901bab55b0f848ec
ForraiFrank <frank.forrai@katonaj-mkszig.hu>
2023-03-22 11:31:41 +0100

    Kaki

---
+++ Forrai_Frank/Szavak.py
@@ -0,0 +1,8 @@
+szo = int(input("Adj meg egy szot! "))
+szo2 = int(input("Adj meg egy szot! "))
+if szo > szo2:
+    print(szo, "nagyobb mint", szo2)
+elif szo < szo2
+    print(szo, "nagyobb mint", szo)
+else
+    print("A kettő szo egyenlő")
---
+++ Forrai_Frank/nyitvatartas.py
@@ -0,0 +1,11 @@
+x = 5
+y = -3
+
+if x < 0 and y < 0:
+    print('mindkettő negatív')
+
+if x < 0 or y < 0:
+    print('van köztük negatív')
+
+if not x <= 0:
+    print('x pozitív')
