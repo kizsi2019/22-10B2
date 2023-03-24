d72119edcea166739c2afc6c6ced4cf221eeb41d
LazarGyorgyLevente <gyorgylevente.lazar@katonaj-mszig.hu>
2023-03-24 08:09:38 +0100

    oop alapelvei

---
+++ Lazar_Gyorgy/fuggveny4.py
@@ -0,0 +1,20 @@
+def kerulet(x, *args):
+
+    if len(args) == 0:
+
+        return 4 * x
+    if len(args)== 1:
+
+        return (x + args[0]) * 2
+
+else:
+
+
+       osszesen = 0
+       for elem in arngs:
+           osszesen = osszesen + elem
+           return x + osszesen
+print(kerulet(2))
+print(kerulet(2,3))
+print(kerulet(2,3,4))
+print(kerulet(2,3,4,5))
---
+++ Lazar_Gyorgy/szotar.py
@@ -0,0 +1,13 @@
+polc = []
+
+polc.append({
+    'szerzo': 'William Golding',
+    'Cim':  'A legyek ura'
+})
+
+polc.append({
+    'szerzo': 'Ottlik Géza',
+    'cim': 'Iskola a határon'
+})
+
+print(polc)
