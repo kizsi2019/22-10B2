4bcc05626cd6967fb84b3881ae34eaf846675447
ParanicsMerci <113499841+ParanicsMerci@users.noreply.github.com>
2023-04-19 11:29:58 +0200

    Add files via upload

---
+++ Paranics_Mercedesz/kedvencfilm.py
@@ -0,0 +1,8 @@
+def óraperc(percek):
+    óra = percek //60
+    perc = percek - óra * 60
+    return str(óra) + 'óra' + str(perc) + 'perc'
+for _ in range(3):
+    cím = input('Add meg egy film címét!')
+    hossz = int(input('Hány perces a film?'))
+    print('A(z)', cím , 'című film', óraperc (hossz),'hosszú')
