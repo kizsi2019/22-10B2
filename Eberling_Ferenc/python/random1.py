e385c601e1b3ae1b8e3f7d440842b0cba4e16023
Nagy Mate Marcell <matemarcell.nagy@katonaj-mkszig.hu>
2023-06-02 08:39:20 +0200

    legnagyobb

---
+++ Eberling_Ferenc/python/random1.py
@@ -0,0 +1,3 @@
+import random
+szam = random.randint(-5, 5)
+print(f'A véletlen szam: {szam}')
---
+++ Nagy_Marcell/modulok2.py
@@ -0,0 +1,5 @@
+from modulok import kerulet
+from modulok import terulet
+
+print(f'Kör kerülete: {kerulet(10):.2F}')
+print(f'Kör területe: {terulet(10):.2F}')
